5f5a4f11047fa15652d069e544229b9277d32dd1
jinh3457 <jinh3457@e4f6ad43-4bc5-4323-aaba-34dce06f1ff6>
2017-02-27 04:33:44 +0000

    refined interface for wishlist. (waiting for databases to be implemented). created empty packages (business, onjects, etc.).
    git-svn-id: file:///home/student/marzonr/groupB/repo@7 e4f6ad43-4bc5-4323-aaba-34dce06f1ff6

--- app/src/main/java/comp3350/budgetapp/presentation/MainActivity.java
+++ app/src/main/java/comp3350/budgetapp/presentation/MainActivity.java
@@ -1,10 +1,11 @@
-package comp3350.budgetapp;
+package comp3350.budgetapp.presentation;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
+
+import comp3350.budgetapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/comp3350/budgetapp/presentation/WishlistActivity.java
+++ app/src/main/java/comp3350/budgetapp/presentation/WishlistActivity.java
@@ -1,10 +1,12 @@
-package comp3350.budgetapp;
+package comp3350.budgetapp.presentation;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import comp3350.budgetapp.R;
 
 public class WishlistActivity extends AppCompatActivity {
 

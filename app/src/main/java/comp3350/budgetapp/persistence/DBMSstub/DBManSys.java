1a8ffc21818715aa9d6f56ab20ffa1a905538dcf
madurom <madurom@e4f6ad43-4bc5-4323-aaba-34dce06f1ff6>
2017-02-28 05:31:28 +0000

    Made changes to DB, now has add, remove and change for wishlist items. It is acting as a collection specifically for wishlist items
    git-svn-id: file:///home/student/marzonr/groupB/repo@21 e4f6ad43-4bc5-4323-aaba-34dce06f1ff6

--- app/src/main/java/comp3350/budgetapp/persistence/DBMSstub/DBManSys.java
+++ app/src/main/java/comp3350/budgetapp/persistence/DBMSstub/DBManSys.java
@@ -1,5 +1,7 @@
 package comp3350.budgetapp.persistence.DBMSstub;
 
+import comp3350.budgetapp.objects.WishListItem;
+
 /**
  * Created by mardelmaduro on 2017-02-27.
  */
@@ -13,6 +15,20 @@ public class DBManSys{
     public void loadDB(){
         //gonna put stuuufff here
         //Placeholder values, when we want to test but don't want to load it ourselves
+
+        WishListItem item;
+
+        item = new WishListItem("laptop",600.00);
+        db.put(item.getItemName(),item);
+
+        item = new WishListItem("phone",400.00);
+        db.put(item.getItemName(),item);
+
+    }
+
+    public void close()
+    {
+        System.out.println(" Closed database ");
     }
 
     public boolean put(String key, Object value){
@@ -33,11 +49,30 @@ public class DBManSys{
         db.remove(key);
     }
 
+
     public String[] getAllKeys(){
         return db.getAllKeys();
     }
 
     public Object[] getAllValues(){
         return db.getAllValues();
+    }
+
+    public String addWishListItem(WishListItem item)
+    {
+        db.put(item.getItemName(),item);
+        return null;
+    }
+
+    public String deleteWishListItem(WishListItem item)
+    {
+        db.remove(item.getItemName());
+        return null;
+    }
+
+    public String updateWishListItem(WishListItem item)
+    {
+        db.put(item.getItemName(),item);
+        return null;
     }
 }

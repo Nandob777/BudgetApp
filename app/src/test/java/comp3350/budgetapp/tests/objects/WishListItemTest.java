a0b39f8cc6b213fa9f7b06d5e76c2ff94f36a906
marzonr <marzonr@e4f6ad43-4bc5-4323-aaba-34dce06f1ff6>
2017-03-19 06:29:51 +0000

    Changed the test cases completely & more thoroughly.
    git-svn-id: file:///home/student/marzonr/groupB/repo@64 e4f6ad43-4bc5-4323-aaba-34dce06f1ff6

--- app/src/test/java/comp3350/budgetapp/tests/objects/WishListItemTest.java
+++ app/src/test/java/comp3350/budgetapp/tests/objects/WishListItemTest.java
@@ -1,85 +1,99 @@
 package comp3350.budgetapp.tests.objects;
 
 import junit.framework.TestCase;
-
 import comp3350.budgetapp.objects.WishListItem;
 
 public class WishListItemTest extends TestCase
 {
-    public static WishListItem nullItem = null;
+    public static WishListItem wish1, wish2, wish3;
     public static final double delta = 0.001;
 
-    private WishListItem createWishListItem(String name, double price)
+
+    public void testWishListWithItemNameOnly()
     {
-        WishListItem item = new WishListItem(name, price);
-        return item;
+        System.out.println("\nStarting testWishListWithItemNameOnly");
+
+        wish1 = new WishListItem("iPhone 9");
+        assertNotNull(wish1);
+        assertEquals("iPhone 9", wish1.getItemName());
+        assertEquals(0.0, wish1.getPrice());
+        assertEquals(0, wish1.getPriority());
+        assertEquals("Item Name: iPhone 9, Current Price with 0% discount: $0.00", wish1.toString());
+
+        wish1.setPrice(900.23);
+        assertEquals(900.23, wish1.getPrice());
+        assertEquals("Item Name: iPhone 9, Current Price with 0% discount: $900.23", wish1.toString());
+
+        wish1.setPriority(3);
+        assertEquals(3, wish1.getPriority());
+
+        wish1.setDiscount(20);
+        assertEquals(720.1840000000001, wish1.getPrice());
+        assertEquals("Item Name: iPhone 9, Current Price with 20% discount: $720.18", wish1.toString());
+
+        System.out.println("Finished testWishListWithItemNameOnly");
     }
 
-    public void testSingleNullItem()
+    public void testWIshListWithItemNameAndPriceOnly()
     {
-        System.out.println("\nStarting testSingleNullItem");
-        assertNull(nullItem);
-        System.out.println("Finished testSingleNullItem");
+        System.out.println("\nStarting testWIshListWithItemNameAndPriceOnly");
+
+        wish1 = new WishListItem("iPhone 10", 1050.65);
+        assertNotNull(wish1);
+        assertEquals("iPhone 10", wish1.getItemName());
+        assertEquals(1050.65, wish1.getPrice());
+        assertEquals(0, wish1.getPriority());
+        assertEquals("Item Name: iPhone 10, Current Price with 0% discount: $1050.65", wish1.toString());
+
+        wish1.setPrice(1029.99);
+        assertEquals(1029.99, wish1.getPrice());
+        assertEquals("Item Name: iPhone 10, Current Price with 0% discount: $1029.99", wish1.toString());
+
+        wish1.setDiscount(25);
+        assertEquals(772.4925000000001, wish1.getPrice());
+        assertEquals("Item Name: iPhone 10, Current Price with 25% discount: $772.49", wish1.toString());
+
+        System.out.println("Finished testWIshListWithItemNameAndPriceOnly");
     }
 
-    public void testSingleInitializedItem()
+    public void testWIshListWithItemNamePriceAndDiscount()
     {
-        System.out.println("\nStarting testSingleInitializedItem");
+        System.out.println("\nStarting testWIshListWithItemNamePriceAndDiscount");
 
-        WishListItem newItem;
+        wish1 = new WishListItem("iPhone 11", 1999.99, 15);
+        assertNotNull(wish1);
+        assertEquals("iPhone 11", wish1.getItemName());
+        assertEquals(1699.9914999999999, wish1.getPrice());
+        assertEquals(0, wish1.getPriority());
+        assertEquals("Item Name: iPhone 11, Current Price with 15% discount: $1699.99", wish1.toString());
 
-        newItem = createWishListItem("iPhone 7+", 900.00);
-        assertNotNull(newItem);
-        assertTrue("iPhone 7+".equals(newItem.getItemName()));
-        assertEquals(900.00,newItem.getPrice(),delta);
+        wish1.setPrice(1599.99);
+        assertEquals(1359.9914999999999, wish1.getPrice());
+        assertEquals("Item Name: iPhone 11, Current Price with 15% discount: $1359.99", wish1.toString());
 
-        assertFalse("".equals(newItem.getItemName()));
-        assertFalse(0 == newItem.getPrice());
+        wish1.setDiscount(25);
+        assertEquals(1019.9936249999998, wish1.getPrice());
+        assertEquals("Item Name: iPhone 11, Current Price with 25% discount: $1019.99", wish1.toString());
 
-        System.out.println("Finished testSingleInitializedItem");
+        System.out.println("Finished testWIshListWithItemNamePriceAndDiscount");
     }
 
-    public void testItemsEqual()
+    public void testEquals()
     {
-        WishListItem item1 = null;
-        WishListItem item2;
-        WishListItem item3;
+        System.out.println("\nStarting testEquals");
+        wish1 = new WishListItem("Samsung S9", 1499.99, 15);
+        wish2 = new WishListItem("Samsung S9", 400.99);
+        wish3 = new WishListItem("Google Pixel");
 
-        String itemName = "iPhone 7+";
-        double itemPrice = 900.00;
+        assertTrue(wish1.equals(wish2));
+        assertFalse(wish1.equals(wish3));
 
-        item1 = createWishListItem(itemName, itemPrice);
+        //wish2 = new WishListItem(null);
+        //assertFalse(wish1.equals(wish2));
 
-        System.out.println("\nStarting testItemsEqual: null v. initialized");
-        assertFalse(item1.equals(nullItem));
-        System.out.println("Finished testItemsEqual: null v. initialized");
+        System.out.println("Finished testEquals");
 
-        System.out.println("\nStarting testItemsEqual: initialized v. initialized (Same item)");
-        item2 = createWishListItem(itemName, itemPrice);
-        assertTrue(item1.equals(item2));
-        assertTrue( (item1.getItemName()).equals(item2.getItemName()) );
-        assertEquals( (item1.getPrice()), (item2.getPrice()), delta);
-        System.out.println("Finished testItemsEqual: initialized v. initialized (Same item)");
-
-        System.out.println("\nStarting testItemsEqual: initialized v. initialized (Diff name)");
-        item2 = createWishListItem("iPhone 6s", itemPrice);
-        assertFalse( item1.equals(item2) );
-        assertFalse( (item1.getItemName()).equals(item2.getItemName()) );
-        assertTrue( (item1.getPrice()) == (item2.getPrice()) );
-        System.out.println("Finished testItemsEqual: initialized v. initialized (Diff name)");
-
-        System.out.println("\nStarting testItemsEqual: initialized v. initialized (Diff price)");
-        item2 = createWishListItem(itemName, 899.00);
-        assertTrue( item1.equals(item2) );
-        assertTrue( (item1.getItemName()).equals(item2.getItemName()) );
-        assertFalse( (item1.getPrice()) == (item2.getPrice()) );
-        System.out.println("Finished testItemsEqual: initialized v. initialized (Diff price)");
-
-        System.out.println("\nStarting testItemsEqual: initialized v. initialized (Diff item)");
-        item2 = createWishListItem("Headphones", 10.00);
-        assertFalse(item1.equals(item2));
-        assertFalse( (item1.getItemName()).equals(item2.getItemName()) );
-        assertFalse( (item1.getPrice()) == (item2.getPrice()) );
-        System.out.println("Finished testItemsEqual: initialized v. initialized (Diff item)");
     }
+
+
 }

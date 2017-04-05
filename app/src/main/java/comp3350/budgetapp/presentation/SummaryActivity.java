776c24c3a942858399c664d4a9e03caf6ba92b28
marzonr <marzonr@e4f6ad43-4bc5-4323-aaba-34dce06f1ff6>
2017-04-05 20:34:29 +0000

    Refactored activity_summary.xml and SummerActivity.java
    git-svn-id: file:///home/student/marzonr/groupB/repo@105 e4f6ad43-4bc5-4323-aaba-34dce06f1ff6

--- app/src/main/java/comp3350/budgetapp/presentation/SummaryActivity.java
+++ app/src/main/java/comp3350/budgetapp/presentation/SummaryActivity.java
@@ -2,24 +2,14 @@ package comp3350.budgetapp.presentation;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.ListView;
 
-import java.util.ArrayList;
+
 
 import comp3350.budgetapp.R;
 import comp3350.budgetapp.business.AccessIncomeSource;
 import comp3350.budgetapp.business.AccessWishListItems;
-import comp3350.budgetapp.business.Calculate;
-import comp3350.budgetapp.objects.Expense;
 import comp3350.budgetapp.business.AccessExpenses;
-import comp3350.budgetapp.objects.WishListItem;
 
 public class SummaryActivity extends Activity{
 
@@ -30,11 +20,7 @@ public class SummaryActivity extends Activity{
     private AccessExpenses accessExpenses;
     private AccessWishListItems accessWishList;
     private AccessIncomeSource accessIncome;
-    private Calculate totalPrice;
 
-
-    private ArrayList<WishListItem> itemList;
-    private WishListItem currentItem;
 
 
     @Override
@@ -49,15 +35,12 @@ public class SummaryActivity extends Activity{
 
 
         viewExpenses = (TextView)findViewById(R.id.viewExpenseTotal);
-        //viewExpenses.setText(accessExpenses.getTotal());
         viewExpenses.setText(ExpenseActivity.total);
 
-        viewWishTotal = (TextView)findViewById(R.id.viewTotal);
-        //viewWishTotal.setText(accessWishList.getTotal());
+        viewWishTotal = (TextView)findViewById(R.id.viewWishlistTotal);
         viewWishTotal.setText(WishlistActivity.total);
 
         viewIncome = (TextView)findViewById(R.id.viewIncomeTotal);
-        //viewIncome.setText(accessIncome.getTotal());
         viewIncome.setText(IncomeActivity.total);
     }
 

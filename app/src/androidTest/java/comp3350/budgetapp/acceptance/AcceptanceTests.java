e26468f6c1dd1dfb323e03d97f2835763f6dc302
badenhow <badenhow@e4f6ad43-4bc5-4323-aaba-34dce06f1ff6>
2017-04-10 03:19:40 +0000

    Removed an unnecesary comment
    git-svn-id: file:///home/student/marzonr/groupB/repo@138 e4f6ad43-4bc5-4323-aaba-34dce06f1ff6

--- app/src/androidTest/java/comp3350/budgetapp/acceptance/AcceptanceTests.java
+++ app/src/androidTest/java/comp3350/budgetapp/acceptance/AcceptanceTests.java
@@ -17,7 +17,6 @@ public class AcceptanceTests
     private static void loadFullTestSuite(){
         suite.addTestSuite(WishlistTest.class);
         suite.addTestSuite(ExpensesTest.class);
-        //Example: suite.addTestSuite(StudentsTest.class);
         // Add all the test suites here
     }
 }
